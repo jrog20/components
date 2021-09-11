37624b7f1a268c6fb7f331fee5623d30ab1fd913
jrog20 <jaimerogstad@gmail.com>
2021-09-11 12:44:16 -0400

    Wrap commentdetail component in approvalcard component

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ import ApprovalCard from './components/ApprovalCard';
 function App() {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail 
-        // author={author} 
-        author="Sara"
-        // time={time} 
-        time="Now"
-        // text={text}
-        text="Cool post"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail 
+          // author={author} 
+          author="Sara"
+          // time={time} 
+          time="Now"
+          // text={text}
+          text="Cool post"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 }
